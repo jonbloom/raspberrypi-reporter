31802ba61927d2eff5f0e9b0df921fd21ea4048c
Jon Bloom <git@jon.tw>
2015-04-16 14:16:45 -0400

    Change get param

--- log.php
+++ log.php
@@ -2,7 +2,7 @@
 include 'connection.php';
 include 'secret.php';
 if ($_GET['pw'] == $pw){
-	$insertSql = "INSERT INTO log (LOG_ID, EMS_ID, TIMESTAMP, BOOKING_ID, RESERVATION_NAME) VALUES (NULL,'{$_GET['room_id']}', NULL, '{$_GET['booking_id']}','{$_GET['booking_name']}')";
+	$insertSql = "INSERT INTO log (LOG_ID, EMS_ID, TIMESTAMP, BOOKING_ID, RESERVATION_NAME) VALUES (NULL,'{$_GET['room_id']}', NULL, '{$_GET['booking_id']}','{$_GET['reserved_by']}')";
 	getConnection()->query($insertSql);
 	echo 'inserted';
 }
