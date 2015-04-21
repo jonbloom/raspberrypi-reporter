dbf184c365a68ac43c34f64d0216a2861f314500
Jon Bloom <git@jon.tw>
2015-04-21 10:54:07 -0400

    timestamp on log as well

--- log.php
+++ log.php
@@ -1,8 +1,9 @@
 <?php
 include 'connection.php';
 include 'secret.php';
+$now = date('Y-m-d H:i:s');
 if ($_GET['pw'] == $pw){
-	$insertSql = "INSERT INTO log (LOG_ID, EMS_ID, TIMESTAMP, BOOKING_ID, RESERVED_BY) VALUES (NULL,'{$_GET['room_id']}', NULL, '{$_GET['booking_id']}','{$_GET['reserved_by']}')";
+	$insertSql = "INSERT INTO log (LOG_ID, EMS_ID, TIMESTAMP, BOOKING_ID, RESERVED_BY) VALUES (NULL,'{$_GET['room_id']}', '{$now}', '{$_GET['booking_id']}','{$_GET['reserved_by']}')";
 	getConnection()->query($insertSql);
 	echo 'inserted';
 }
