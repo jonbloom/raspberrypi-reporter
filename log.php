30595fbd4fb0580f76fc44b4f47026c60d94ac9c
Jon Bloom <git@jon.tw>
2015-04-21 09:53:20 -0400

    Fix column name

--- log.php
+++ log.php
@@ -2,7 +2,8 @@
 include 'connection.php';
 include 'secret.php';
 if ($_GET['pw'] == $pw){
-	$insertSql = "INSERT INTO log (LOG_ID, EMS_ID, TIMESTAMP, BOOKING_ID, RESERVATION_NAME) VALUES (NULL,'{$_GET['room_id']}', NULL, '{$_GET['booking_id']}','{$_GET['reserved_by']}')";
+	$insertSql = "INSERT INTO log (LOG_ID, EMS_ID, TIMESTAMP, BOOKING_ID, RESERVED_BY) VALUES (NULL,'{$_GET['room_id']}', NULL, '{$_GET['booking_id']}','{$_GET['reserved_by']}')";
+	echo $sql;
 	getConnection()->query($insertSql);
 	echo 'inserted';
 }
