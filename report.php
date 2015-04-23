b1fdf92a326edbd003585f4aed28edfe6dbb9e96
Jon Bloom <git@jon.tw>
2015-04-23 10:10:26 -0400

    Quotes around timestamp values

--- report.php
+++ report.php
@@ -11,11 +11,11 @@ if ($_GET['pw'] == $pw){
 	$selectSql = "SELECT 1 FROM raspberrypi WHERE MAC_ADDRESS = '" . $_GET['mac'] . "'";
 	$exists = getConnection()->query($selectSql);
 	if ($exists->num_rows){
-		$updateSql = "UPDATE raspberrypi SET IP_ADDRESS = '{$_GET['ip']}', ROOM_NUMBER = '{$rooms[$_GET['room_id']]}', EMS_ID = '{$_GET['room_id']}', LAST_UPDATED = {$now} WHERE MAC_ADDRESS = '{$_GET['mac']}'";
+		$updateSql = "UPDATE raspberrypi SET IP_ADDRESS = '{$_GET['ip']}', ROOM_NUMBER = '{$rooms[$_GET['room_id']]}', EMS_ID = '{$_GET['room_id']}', LAST_UPDATED = '{$now}' WHERE MAC_ADDRESS = '{$_GET['mac']}'";
 		getConnection()->query($updateSql);
 		echo 'updated';
 	} else {
-		$insertSql = "INSERT INTO raspberrypi (MAC_ADDRESS, IP_ADDRESS, ROOM_NUMBER, EMS_ID, LAST_UPDATED) VALUES ('{$_GET['mac']}', '{$_GET['ip']}', '{$rooms[$_GET['room_id']]}', '{$_GET['room_id']}', {$now})";
+		$insertSql = "INSERT INTO raspberrypi (MAC_ADDRESS, IP_ADDRESS, ROOM_NUMBER, EMS_ID, LAST_UPDATED) VALUES ('{$_GET['mac']}', '{$_GET['ip']}', '{$rooms[$_GET['room_id']]}', '{$_GET['room_id']}', '{$now}')";
 		getConnection()->query($insertSql);
 		echo 'inserted';
 	}
