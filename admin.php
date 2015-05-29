98345d5977b5421d1ffbdbc84536cf7436d4a809
Jon Bloom <git@jon.tw>
2015-05-29 08:26:40 -0400

    Order Pis by Room Number

--- admin.php
+++ admin.php
@@ -28,17 +28,18 @@ include 'connection.php';
 			</thead>
 			<tbody>
 			<?php
-				$sql = 'SELECT ROOM_NUMBER, EMS_ID, IP_ADDRESS, MAC_ADDRESS, LAST_UPDATED FROM raspberrypi';
-				$res = getConnection()->query($sql);
-				while ($pi = $res->fetch_assoc()){ ?>
+$sql = 'SELECT ROOM_NUMBER, EMS_ID, IP_ADDRESS, MAC_ADDRESS, LAST_UPDATED FROM raspberrypi ORDER BY ROOM_NUMBER';
+$res = getConnection()->query($sql);
+while ($pi = $res->fetch_assoc()) {?>
 					<tr>
-						<td><?php echo $pi['ROOM_NUMBER']; ?></td>
-						<td><?php echo $pi['EMS_ID']; ?></td>
-						<td><?php echo $pi['IP_ADDRESS']; ?></td>
-						<td><?php echo $pi['MAC_ADDRESS']; ?></td>
-						<td><?php echo $pi['LAST_UPDATED']; ?></td>
+						<td><?php echo $pi['ROOM_NUMBER'];?></td>
+						<td><?php echo $pi['EMS_ID'];?></td>
+						<td><?php echo $pi['IP_ADDRESS'];?></td>
+						<td><?php echo $pi['MAC_ADDRESS'];?></td>
+						<td><?php echo $pi['LAST_UPDATED'];?></td>
 					</tr>
-				<?php } ?>			
+				<?php }
+?>
 			</tbody>
 		</table>
 		<h2>Log</h2>
@@ -53,16 +54,17 @@ include 'connection.php';
 			</thead>
 			<tbody>
 			<?php
-				$sql = 'SELECT raspberrypi.ROOM_NUMBER, log.TIMESTAMP, log.BOOKING_ID, log.RESERVED_BY FROM raspberrypi, log WHERE raspberrypi.EMS_ID = log.EMS_ID ORDER BY log.TIMESTAMP';
-				$res = getConnection()->query($sql);
-				while ($log = $res->fetch_assoc()){ ?>
+$sql = 'SELECT raspberrypi.ROOM_NUMBER, log.TIMESTAMP, log.BOOKING_ID, log.RESERVED_BY FROM raspberrypi, log WHERE raspberrypi.EMS_ID = log.EMS_ID ORDER BY log.TIMESTAMP';
+$res = getConnection()->query($sql);
+while ($log = $res->fetch_assoc()) {?>
 					<tr>
-						<td><?php echo $log['ROOM_NUMBER']; ?></td>
-						<td><?php echo $log['TIMESTAMP']; ?></td>
-						<td><?php echo $log['BOOKING_ID']; ?></td>
-						<td><?php echo $log['RESERVED_BY']; ?></td>
+						<td><?php echo $log['ROOM_NUMBER'];?></td>
+						<td><?php echo $log['TIMESTAMP'];?></td>
+						<td><?php echo $log['BOOKING_ID'];?></td>
+						<td><?php echo $log['RESERVED_BY'];?></td>
 					</tr>
-				<?php } ?>			
+				<?php }
+?>
 			</tbody>
 		</table>
 	</body>
